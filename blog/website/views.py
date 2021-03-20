9bf4e5de3161e275c43f281c9fe637def0064854
Clecio <clecio.antao@gmail.com>
2021-03-20 15:28:55 -0300

    Aula sobre comandos sistema templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello_blog(request):
+    lista = ['lista','lista2','lista3','lista4','Django']
+    data = {'name': 'Curso Django', 'lista_tec': lista}
+    
+    return render(request, 'index.html', data)
+
